96b1745eb4cad3a07e3dacf80d64134222e7aa74
TacB0sS <adam.vdk@nu-art-software.com>
2019-06-13 13:43:38 +0300

    logger with configuration

--- src/main/java/com/nu/art/cyborg/demo/CyborgDemoApplication.java
+++ src/main/java/com/nu/art/cyborg/demo/CyborgDemoApplication.java
@@ -13,16 +13,21 @@ package com.nu.art.cyborg.demo;
 import android.app.Application;
 import android.os.Environment;
 
+import com.nu.art.belog.BeConfig;
 import com.nu.art.belog.BeLogged;
+import com.nu.art.core.exceptions.runtime.BadImplementationException;
+import com.nu.art.core.interfaces.Getter;
+import com.nu.art.core.tools.StreamTools;
 import com.nu.art.cyborg.core.CyborgBuilder;
 import com.nu.art.cyborg.core.CyborgBuilder.CyborgConfiguration;
 import com.nu.art.cyborg.core.CyborgController;
 import com.nu.art.cyborg.core.CyborgStackController;
 import com.nu.art.cyborg.core.abs._DebugFlags;
-import com.nu.art.cyborg.core.modules.AndroidLogger;
+import com.nu.art.cyborg.core.loggers.AndroidLogger;
 import com.nu.art.cyborg.demo.model.MyModulePack;
 
 import java.io.File;
+import java.io.IOException;
 
 import static com.nu.art.belog.loggers.FileLogger.Config_FastFileLogger;
 import static com.nu.art.belog.loggers.FileLogger.LogConfig_FileLogger;
@@ -38,15 +43,21 @@ public class CyborgDemoApplication
 		CyborgController.DebugFlag.enable();
 		_DebugFlags.Debug_Performance.enable();
 
-		File logFolder = new File(Environment.getExternalStorageDirectory() + "/Downloads/cyborg-logs");
-		LogConfig_FileLogger.setFolder(logFolder).setFileName("log-file");
+		Getter<BeConfig> logConfigLoader = new Getter<BeConfig>() {
+			@Override
+			public BeConfig get() {
+				try {
+					String configAsString = StreamTools.readFullyAsString(getResources().openRawResource(R.raw.log_config));
+					return (BeConfig) BeLogged.getInstance().getSerializer().deserialize(configAsString, BeConfig.class);
+				} catch (IOException e) {
+					throw new BadImplementationException("Unable to deserialize log config");
+				}
+			}
+		};
 
-		//		CyborgBuilder.startCyborg(new CyborgConfiguration(this).setLaunchConfiguration(R.layout.v1_activity__injection_example));
-		//		CyborgBuilder.startCyborg(new CyborgConfiguration(this).setLaunchConfiguration(R.layout.v1_activity__matrix_tester_example, MyModulePack.class));
-		//		CyborgBuilder.startCyborg(new CyborgConfiguration(this).setLaunchConfiguration(R.layout.v1_activity__dynamic_controllers_stack_example, MyModulePack.class));
-		CyborgConfiguration configuration = new CyborgConfiguration(this).setLaunchConfiguration(R.layout.v1_activity__examples_selection)
-		                                                                 .setModulesPacks(MyModulePack.class)
-		                                                                 .setLogConfig(AndroidLogger.Config_FastAndroidLogger.merge(Config_FastFileLogger));
-		CyborgBuilder.startCyborg(configuration);
+		CyborgBuilder.startCyborg(new CyborgConfiguration(this)
+			                          .setLaunchConfiguration(R.layout.v1_activity__examples_selection)
+			                          .setModulesPacks(MyModulePack.class)
+			                          .setLogConfig(logConfigLoader));
 	}
 }
